a9c779b0e63a25ffe3fd8fdc49e6ebdf86c8d64e
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-03-08 10:53:41 +0100

    .

---
+++ Microwave.Test.Integration/SomeINtegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class SomeINtegrationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize objects that are needed
+        }
+    }
+}
